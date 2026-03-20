fb8c0bedc1c96346032495ada0f8ea149e9db6b9
hoshi lol <thehoshiik@gmail.com>
2026-03-21 01:00:00 +0400

    Add project files.

---
+++ SQLConnection/DataAccessManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace SQLConnection
+{
+    public class DataAccessManager
+    {
+        private string _connectionstring;
+
+        public DataAccessManager(string ServerName, string DBName)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = ServerName;
+            builder.InitialCatalog = DBName;
+            builder.TrustServerCertificate = true;
+            builder.IntegratedSecurity = true;
+
+            _connectionstring = builder.ConnectionString;
+        }
+    }
+}
---
+++ SQLConnection/Tables/Branches.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConnection
+{
+    public class Branches
+    {
+        public int BranchID { get; set; }
+        public int ManagerID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
