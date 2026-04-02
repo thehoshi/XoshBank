00be97c4ad8808cee58a5d6ad0a2d58497971058
sadikhlileyla <sadikhlileyla@gmail.com>
2026-04-02 14:31:27 +0400

    Added MsSQLAccountRepository(Delet,Update,Insert,GetById,GetAll)

--- XoshBank.Persistent.SQLServer/Repositories/MsSQLAccountRepository.cs
+++ XoshBank.Persistent.SQLServer/Repositories/MsSQLAccountRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Security.Principal;
+using XoshBank.Entities;
 using XoshBankCore;
 using XoshBankCore.Entities.Repositories;
 
@@ -14,22 +16,32 @@ namespace XoshBank.Persistent.SQLServer.Repositories
         {
             _connectionString = connectionString;
         }
+        #region Delete
         public void Delete(int id)
         {
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-                var command = new SqlCommand(
-                    "UPDATE Account SET DeletedAt = @DeletedAt WHERE Id = @Id AND DeletedAt IS NULL",
-                    connection);
+                string query = "UPDATE Account SET DeletedAt = @DeletedAt WHERE Id = @Id AND DeletedAt IS NULL";
+
+                var command = new SqlCommand(query,connection);
 
                 command.Parameters.AddWithValue("@DeletedAt", DateTime.Now);
                 command.Parameters.AddWithValue("@Id", id);
 
                 command.ExecuteNonQuery();
+
+                int rowsCount = command.ExecuteNonQuery();
+
+                if (rowsCount != 1)
+                    throw new Exception("Something went wrong while updating user");
+                else
+                    Console.WriteLine("The operation was completed successfully");
             }
         }
+        #endregion
 
+        #region GetAll
         public List<Account> GetAll()
         {
             var accounts = new List<Account>();
@@ -37,7 +49,8 @@ namespace XoshBank.Persistent.SQLServer.Repositories
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-                using (var command = new SqlCommand("SELECT * FROM Account", connection))
+                string query = "SELECT * FROM Account";
+                using (var command = new SqlCommand(query, connection))
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
@@ -53,28 +66,117 @@ namespace XoshBank.Persistent.SQLServer.Repositories
                             CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
                             DeletedAt = Convert.ToDateTime(reader["DeletedAt"]),
                         };
-
+                        
                         accounts.Add(account);
                     }
+                    int rowsCount = command.ExecuteNonQuery();
+                    if (rowsCount != 1)
+                        throw new Exception("Something went wrong while updating user");
+                    else
+                        Console.WriteLine("The operation was completed successfully");
                 }
             }
             return accounts;
         }
+        #endregion
 
+        #region GetById
         public Account GetById(int id)
         {
-            throw new NotImplementedException();
-        }
+            using (SqlConnection connection = new SqlConnection(_connectionString)) 
+            {
+                connection.Open();
+                string query = "SELECT * FROM Account WHERE Id = @Id AND DeletedAt IS NULL";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Account
+                            {
+                                ID = Convert.ToInt32(reader["AccountsID"]),
+                                CustomerID = Convert.ToInt32(reader["CustomerID"]),
+                                AccountNumber = reader["AccountNumber"] as string,
+                                Balance = Convert.ToDecimal(reader["Balance"]),
+                                AccountType = reader["AccountType"] as string,
+                                Currency = reader["Currency"] as string,
+                                CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+                                DeletedAt = Convert.ToDateTime(reader["DeletedAt"]),
+                            };
+                        }
 
-        public void Insert(Account entity)
-        {
-            throw new NotImplementedException();
-        }
+                        int rowsCount = command.ExecuteNonQuery();
 
-        public void Update(Account entity)
-        {
-            throw new NotImplementedException();
+                        if (rowsCount != 1)
+                            throw new Exception("Something went wrong while updating user");
+                        else
+                            Console.WriteLine("The operation was completed successfully");
+                    }
+                }
+            }
+            return null;
         }
+        #endregion
+
+        #region Insert
+        public void Insert(Account account)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Accounts (CustomerID,AccountNumber,Balance,AccountType,Curency,CreatedAt,DeletedAt) " +
+                    "VALUES(@CustomerID,@AccountNumber,@Balance,@AccountType,@Curency,@CreatedAt,) ";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", account.CustomerID);
+                    command.Parameters.AddWithValue("@AccountNumber", account.AccountNumber);
+                    command.Parameters.AddWithValue("@Balance", account.Balance);
+                    command.Parameters.AddWithValue("@AccountType", account.AccountType);
+                    command.Parameters.AddWithValue("Curency", account.Currency);
+                    command.Parameters.AddWithValue("@CreatedAt", account.CreatedAt);
+                    command.Parameters.AddWithValue("@DeletedAt",account.DeletedAt);
+
+                    int rowsCount = command.ExecuteNonQuery();
+                    if (rowsCount != 1)
+                        throw new Exception("Something went wrong while updating user");
+                    else
+                        Console.WriteLine("The operation was completed successfully");
+                }
+            }
+        }
+        #endregion
+
+        #region Update
+        public void Update(Account account)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE Cards SET CustomerID = @CustomerID, AccountNumber = @AccountNumber, Balance = @Balance, AccountType = @AccountType, Curency = @Curency " +
+                    "CreatedAt = @CreatedAt, DeletedAt = @DeletedAt WHERE AccountID = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", account.CustomerID);
+                    command.Parameters.AddWithValue("@AccountNumber", account.AccountNumber);
+                    command.Parameters.AddWithValue("@Balance", account.Balance);
+                    command.Parameters.AddWithValue("@AccountType", account.AccountType);
+                    command.Parameters.AddWithValue("@Curency", account.Currency);
+                    command.Parameters.AddWithValue("@CreatedAt", account.CreatedAt);
+                    command.Parameters.AddWithValue("@DeletedAt", account.DeletedAt);
+
+                    int rowsCount = command.ExecuteNonQuery();
+
+                    if (rowsCount != 1)
+                        throw new Exception("Something went wrong while updating user");
+                    else
+                        Console.WriteLine("The operation was completed successfully");
+                }
+            }
+        }
+        #endregion
     }
 }
 
