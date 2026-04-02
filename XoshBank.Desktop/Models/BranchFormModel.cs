b0c8dec5b0e9c55c50bf454fdb5f82d5febff8da
hoshi lol <thehoshiik@gmail.com>
2026-04-03 03:10:23 +0400

    Added BranchesControl and LoansControl. Also created View, UserControl, Models folders. swag

--- XoshBank.Desktop/Models/BranchFormModel.cs
+++ XoshBank.Desktop/Models/BranchFormModel.cs
@@ -12,7 +12,7 @@ namespace XoshBank.Models
         public string City { get; set; }
         public string Address { get; set; }
         public string ManagerName { get; set; }
-        public string PhoneNUmber { get; set; }
+        public string PhoneNumber { get; set; }
         public int? EmployeeCount { get; set; }
         public DateTime? OpeningDate { get; set; }
         public double? Revenue { get; set; }
