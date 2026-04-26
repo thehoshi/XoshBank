995379ba9c0ad7bb84c365272ddaa86b2120f42d
sadikhlileyla <sadikhlileyla@gmail.com>
2026-04-26 21:22:52 +0400

    Implement command pattern for AddCustomerCommand

--- XoshBank.Desktop/Command/Customers/AddCustomerCommand.cs
+++ XoshBank.Desktop/Command/Customers/AddCustomerCommand.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XoshBank.Desktop.ViewModels;
+using XoshBank.Enums;
 
 namespace XoshBank.Command.Customers
 {
     public class AddCustomerCommand
     {
 
+        private readonly CustomerControlViewModel _viewModel;
+        public AddCustomerCommand(CustomerControlViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            _viewModel.CurrentState = ViewState.Add;
+        }
     }
 }
