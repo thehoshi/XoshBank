90efbae80c1914d96584255366b5e340f3b096f0
thehoshi <thehoshiik@gmail.com>
2026-06-03 02:54:23 +0400

    BranchController finished

--- XoshBank.Web/Controllers/BranchesController.cs
+++ XoshBank.Web/Controllers/BranchesController.cs
@@ -1,6 +1,28 @@
-﻿namespace XoshBank.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using XoshBank.Web.Models;
+using XoshBank.Web.Services.Interfaces;
+
+namespace XoshBank.Web.Controllers
 {
-    public class BranchesController
+    public class BranchesController : Controller
     {
+        private readonly IBranchService _branchService;
+
+        public BranchesController(IBranchService branchService)
+        {
+            _branchService = branchService;
+        }
+
+        public IActionResult Index()
+        {
+            var branchModels = _branchService.Get();
+
+            var branchUiModel = new BranchUiModel
+            {
+                Branches = branchModels
+            };
+
+            return View(branchUiModel);
+        }
     }
 }
